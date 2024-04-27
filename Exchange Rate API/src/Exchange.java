d829898455be59133fc6755f8ee17891a0dd5463
jadelcruz1 <jardelcruz6@gmail.com>
2024-04-26 21:33:06 -0300

    make the class Exchange

--- Exchange Rate API/src/Exchange.java
+++ Exchange Rate API/src/Exchange.java
@@ -1,6 +1,26 @@
 public class Exchange {
 
-    private String conversionRrates;
+    private int numberOptions;
+    private double value;
 
+    public Exchange(int numberOptions, double value) {
+        this.numberOptions = numberOptions;
+        this.value = value;
+    }
 
+    public int getNumberOptions() {
+        return numberOptions;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Exchange{" +
+                "numberOptions=" + numberOptions +
+                ", value=" + value +
+                '}';
+    }
 }
